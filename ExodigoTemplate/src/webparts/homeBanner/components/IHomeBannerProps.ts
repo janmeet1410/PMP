e646fec80e78dd6cc346d86a1377cc0ce69533d9
janmeet1410 <padiyajanmeet6@gmail.com>
2024-06-26 23:35:05 +0530

    webpart added

---
+++ ExodigoTemplate/src/webparts/homeBanner/components/IHomeBannerProps.ts
@@ -0,0 +1,7 @@
+export interface IHomeBannerProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ ExodigoTemplate/src/webparts/homeBanner/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IHomeBannerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'HomeBannerWebPartStrings' {
+  const strings: IHomeBannerWebPartStrings;
+  export = strings;
+}
